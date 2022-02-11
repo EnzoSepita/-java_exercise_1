ce988be47c316f64bc5ba9636e70cea682739f02
Enzo Souane <enzo.souane@epita.fr>
2022-02-11 10:25:43 +0100

    ok

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Launcher {
+import java.util.Scanner;
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Bienvenu");
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Ecrit quelque chose");
+        String rep = scanner.nextLine();
+        if (!rep.equals("quit")) {
+            System.out.println("Unknown command");
+        }
+
+    }
 }
